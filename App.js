dfc0faaefcaf13d0627a24df1a95ff5e3a79c577
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2023-12-05 10:23:53 +0500

    set up firebase in android and ios successfully

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import 'react-native-gesture-handler';
 import {View, Text, SafeAreaView, Button} from 'react-native';
 import React from 'react';
 import ScreenComponent from './app/components/ScreenComponent';
+import colors from './app/styles/colors';
 
 export default function App() {
   return (
     <ScreenComponent>
       <View>
-        <Text>App file</Text>
+        <Text>App file is not complete</Text>
       </View>
     </ScreenComponent>
   );
