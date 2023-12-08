843e1a2bd946eaf4181d9574a40c8bc6210ab0ca
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2023-12-08 15:03:17 +0500

    add splash screen successfully!

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNavigator from './app/navigation/MainNavigator';
 import {ThemeProvider} from './app/themes/ThemeContext';
+import SplashScreen from './app/screens/SplashScreen';
 
 export default function App() {
+  const [splashDone, setSplashDone] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashDone(true);
+    }, 2000);
+  }, []);
   return (
     <>
       <ThemeProvider>
-        <MainNavigator />
+        {splashDone ? <MainNavigator /> : <SplashScreen />}
       </ThemeProvider>
     </>
   );
