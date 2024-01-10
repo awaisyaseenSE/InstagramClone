a7f80af66da68b253adf151258a333c7cc20055a
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-01-10 10:45:53 +0500

    before adding video call feature

--- app/screens/Profile/ProfileUserTagsCompo.js
+++ app/screens/Profile/ProfileUserTagsCompo.js
@@ -1,10 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useTheme} from '../../themes/ThemeContext';
 
 const ProfileUserTagsCompo = () => {
+  const {theme} = useTheme();
   return (
-    <View>
-      <Text>ProfileUserTagsCompo</Text>
+    <View style={{alignItems: 'center'}}>
+      <Text style={{color: theme.text, fontSize: 14, marginTop: 12}}>
+        Profile User Tags Compo
+      </Text>
     </View>
   );
 };
