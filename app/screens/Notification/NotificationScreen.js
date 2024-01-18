82d6d4d3d46d61800b365ae445ed0ba66e382258
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-01-18 17:14:29 +0500

    show notifications in notification screen and navigate to user screens successfully

--- app/screens/Notification/NotificationScreen.js
+++ app/screens/Notification/NotificationScreen.js
@@ -1,57 +1,42 @@
-import {View, Text, TouchableOpacity, FlatList} from 'react-native';
-import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import ScreenComponent from '../../components/ScreenComponent';
 import TopCompoWithHeading from '../../components/TopCompoWithHeading';
 import {useNavigation} from '@react-navigation/native';
 import navigationStrings from '../../navigation/navigationStrings';
 import {useTheme} from '../../themes/ThemeContext';
-import ButtonComponent from '../CreateAccount/components/ButtonComponent';
-import {
-  sendSingleNotification,
-  sendNotificationToAll,
-} from '../../utils/sendNotification';
 import auth from '@react-native-firebase/auth';
 import colors from '../../styles/colors';
-import {onDisplayNotificationNotifee} from '../../utils/notifeeHandler';
+import firestore from '@react-native-firebase/firestore';
+import MyIndicator from '../../components/MyIndicator';
+import ShowNotificationCompo from '../components/ShowNotificationCompo';
 
 export default function NotificationScreen() {
   const {theme} = useTheme();
   const navigation = useNavigation();
   const [loading, setLoading] = useState(false);
+  const [notifications, setNotifications] = useState([]);
 
-  // senderID,
-  // receiverID,
-  // title,
-  // body,
-  // imageUrl,
-  // type,
-  // classID,
-  const senderID = auth().currentUser?.uid;
-  const receiverID = 'r1WHmkg9OCQUAH5Y750AQMl9kdw1';
-  const title = 'Title This is a Notification for Testing';
-  const body = 'body of notification is here';
-  const imageUrl =
-    'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_640.jpg';
-  const type = 'post';
-  const typeID = 'ksjfkljsdl';
-  const fcmToken =
-    'f1EEx0igRYKZ-byIo_WFFt:APA91bHYDdI8WZdXRofLoFdlOkJ4E6AwVVC8gSRO9R_DGnHxUgAQJ5UICn-BS7vst79sIXbf9KiAGSrCrosTTDprMecUT8GEojEye6wmjPK8GFWsaaCrxgaXAbTv5Ajl9mUMsWyF5B8F';
-  const handleNotification = async () => {
-    await sendSingleNotification(
-      senderID,
-      receiverID,
-      title,
-      body,
-      imageUrl,
-      type,
-      typeID,
-      fcmToken,
-    );
-  };
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = firestore()
+      .collection('notifications')
+      .orderBy('time', 'desc')
+      .onSnapshot(snap => {
+        const allNotificationsData = snap.docs.map(doc => ({...doc.data()}));
+        const filteredNotificationsData = allNotificationsData.filter(
+          ele => ele.receiverID === auth().currentUser?.uid,
+        );
+        // console.log(
+        //   'Total Notification is: ',
+        //   filteredNotificationsData.length,
+        // );
+        setNotifications(filteredNotificationsData);
+        setLoading(false);
+      });
 
-  const handleSendAllNotfication = () => {
-    sendNotificationToAll(title, body, imageUrl, type, typeID);
-  };
+    return () => unsubscribe();
+  }, []);
 
   return (
     <>
@@ -60,28 +45,27 @@ export default function NotificationScreen() {
           title="Notification"
           onPress={() => navigation.goBack()}
         />
-        <View style={{alignItems: 'center', marginTop: 12}}>
-          <ButtonComponent
-            title="Send Notification"
-            onPress={() => handleNotification()}
-            style={{width: '70%', marginVertical: 14}}
-          />
-          <ButtonComponent
-            title="Send Notification To All"
-            onPress={() => handleSendAllNotfication()}
-            style={{backgroundColor: colors.lightBlack, width: '70%'}}
-          />
-          <ButtonComponent
-            title="Notifee display Notification"
-            onPress={() => onDisplayNotificationNotifee()}
-            style={{
-              backgroundColor: colors.lightGrey,
-              width: '70%',
-              marginTop: 12,
-            }}
+        <View style={styles.container}>
+          <FlatList
+            data={notifications}
+            renderItem={({item}) => <ShowNotificationCompo data={item} />}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item, index) => index.toString()}
           />
         </View>
       </ScreenComponent>
+      <MyIndicator
+        visible={loading}
+        backgroundColor={theme.loginBackground}
+        size={'large'}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 12,
+  },
+});
