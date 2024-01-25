9c38462fc9aa95575562c237edfe586b6b8d6803
awaisyaseenSE <148967337+awaisyaseenSE@users.noreply.github.com>
2024-01-25 16:02:09 +0500

    add goeloaction library and get longitude and lattitude for android

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -8,6 +8,8 @@ import firestore from '@react-native-firebase/firestore';
 import MyIndicator from '../components/MyIndicator';
 import ShowPostsCompo from './components/ShowPostsCompo';
 import BottomSheetComponent from '../components/BottomSheetComponent';
+import {getLocation} from '../utils/getUserLocation';
+import fontFamily from '../styles/fontFamily';
 
 export default function Home({switchToScreen}) {
   const {theme} = useTheme();
@@ -32,6 +34,59 @@ export default function Home({switchToScreen}) {
       });
 
     return () => unsubscribe();
+  }, []);
+
+  const getCityAndCountry = async (latitude, longitude) => {
+    try {
+      // const response = await fetch(
+      //   `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCtRP6bt1pJv3u2LXVLge9d34EMbz2AbAk`,
+      // );
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`,
+      );
+      const data = await response.json();
+      if (data.status === 'OK') {
+        const addressComponents = data.results[0].address_components;
+        let city = '';
+        let country = '';
+        for (let component of addressComponents) {
+          if (component.types.includes('locality')) {
+            city = component.long_name;
+          } else if (component.types.includes('country')) {
+            country = component.long_name;
+          }
+        }
+        return {city, country};
+      } else {
+        throw new Error('Failed to fetch location information');
+      }
+    } catch (error) {
+      throw new Error('Error fetching location information: ' + error.message);
+    }
+  };
+
+  const handleGetLocation = async () => {
+    try {
+      const position = await getLocation();
+
+      if (position !== undefined && position !== null) {
+        console.log('Location of user is: ', position);
+        const {latitude, longitude} = position.coords;
+        try {
+          const locationInfo = await getCityAndCountry(latitude, longitude);
+          console.log('City:', locationInfo.city);
+          console.log('Country:', locationInfo.country);
+        } catch (error) {
+          console.error('Error in getting user city name in home.js ', error);
+        }
+      }
+    } catch (error) {
+      console.log('Error in handleGetLocation in Home screen: ', error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetLocation();
   }, []);
 
   return (
